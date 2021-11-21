594f84eae89d604c83cbfd2ccfbd4efcc57a4fbb
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:05:29 +0000

    Added ?ExtensionInterface

--- EveryWorkflowAdminPanelBundle.php
+++ EveryWorkflowAdminPanelBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\AdminPanelBundle;
 
 use EveryWorkflow\AdminPanelBundle\DependencyInjection\AdminPanelExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowAdminPanelBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new AdminPanelExtension();
     }
